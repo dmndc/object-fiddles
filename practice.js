4ae7eee9fbcee90731d0191e0d5e8ecc9b4eb9dd
Dino Mandic <dinomandic@hotmail.com>
2017-10-10 15:17:03 -0500

    my first commit

--- practice.js
+++ practice.js
@@ -6,8 +6,12 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+const me = {
+  name: 'Dino',
+  age: 32
+}
 
+alert(me.age);
 
 
 
@@ -20,7 +24,14 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+const favoriteThings = {
+  band: 'Rolling Stones',
+  food: 'Bacon',
+  person: 'Mother',
+  book: 'Black Swan by Taleb',
+  movie: 'Apocalypse Now', 
+  holiday: 'Croatia'
+}
 
 
 
@@ -30,15 +41,16 @@
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
-
+favoriteThings.car = 'BMW';
+favoriteThings.brand = 'Giuseppe Zanotti';
 
 
 
 
 //Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 
-  //Code here
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings.book = 'Harry Potter';
 
 
 
@@ -58,7 +70,10 @@ Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. 
 */
 
-  //Code here
+const backPack = {}
+var item = 'firstPocket';
+
+backpack[item] = 'chapstick';
 
 
 
@@ -67,7 +82,7 @@ that is named color, with the value being the color of your backpack.
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+alert(backPack);
 
  
 
@@ -77,7 +92,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-  //Code here
+console.log(backPack);
 
 
 
@@ -99,7 +114,8 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 
 
@@ -121,7 +137,7 @@ var user2 = {
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+const methodCollection = {};
 
 
 
